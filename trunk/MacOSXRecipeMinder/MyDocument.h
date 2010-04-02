9e82828152d8263e88d961ca65b53fdaf42b39a4
chancebatwalrus <chancebatwalrus@2fdbceb2-63f0-825b-eaa0-fe54d10d31b1>
2010-04-02 18:58:05 +0000

    added macosx gui app

---
+++ trunk/MacOSXRecipeMinder/MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  RecipeMinder
+//
+//  Created by Pascale, Chancellor T. on 4/2/10.
+//  Copyright __MyCompanyName__ 2010 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument {
+}
+
+@end
---
+++ trunk/MacOSXRecipeMinder/MyDocument.m
@@ -0,0 +1,33 @@
+//
+//  MyDocument.m
+//  RecipeMinder
+//
+//  Created by Pascale, Chancellor T. on 4/2/10.
+//  Copyright __MyCompanyName__ 2010 . All rights reserved.
+//
+
+#import "MyDocument.h"
+
+@implementation MyDocument
+
+- (id)init 
+{
+    self = [super init];
+    if (self != nil) {
+        // initialization code
+    }
+    return self;
+}
+
+- (NSString *)windowNibName 
+{
+    return @"MyDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *)windowController 
+{
+    [super windowControllerDidLoadNib:windowController];
+    // user interface preparation code
+}
+
+@end
