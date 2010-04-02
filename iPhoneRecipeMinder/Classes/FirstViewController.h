4552b60982839a5314613689aa0423d4a47cc53c
chancebatwalrus <chancebatwalrus@2fdbceb2-63f0-825b-eaa0-fe54d10d31b1>
2010-04-02 18:45:49 +0000

    Adding Iphone/ipad version of software

---
+++ iPhoneRecipeMinder/Classes/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  RecipeMinder
+//
+//  Created by Pascale, Chancellor T. on 4/2/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController {
+
+}
+
+@end
---
+++ iPhoneRecipeMinder/Classes/RecipeMinderAppDelegate.m
@@ -0,0 +1,45 @@
+//
+//  RecipeMinderAppDelegate.m
+//  RecipeMinder
+//
+//  Created by Pascale, Chancellor T. on 4/2/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import "RecipeMinderAppDelegate.h"
+
+
+@implementation RecipeMinderAppDelegate
+
+@synthesize window;
+@synthesize tabBarController;
+
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+    
+    // Add the tab bar controller's current view as a subview of the window
+    [window addSubview:tabBarController.view];
+}
+
+
+/*
+// Optional UITabBarControllerDelegate method
+- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController {
+}
+*/
+
+/*
+// Optional UITabBarControllerDelegate method
+- (void)tabBarController:(UITabBarController *)tabBarController didEndCustomizingViewControllers:(NSArray *)viewControllers changed:(BOOL)changed {
+}
+*/
+
+
+- (void)dealloc {
+    [tabBarController release];
+    [window release];
+    [super dealloc];
+}
+
+@end
+
